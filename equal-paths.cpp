57b47f34a31c182991b5b87796e634b866c4d373
Lubbock Xu <lubbockx@usc.edu>
2025-11-15 21:09:11 +0000

    Complete hw4: updated equal-paths.cpp and equal-paths.h

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -13,6 +13,36 @@ using namespace std;
 bool equalPaths(Node * root)
 {
     // Add your code below
+  if(root == NULL)
+  {
+    return true;
+  }
 
+  int leafDepth = -1;
+  return helper(root, 0, leafDepth);
+}
+
+bool helper(Node* currNode, int depth, int& leafDepth)
+{
+  //true if tree is empty
+  if(currNode == NULL)
+  {
+    return true;
+  }
+
+  if(currNode->left == NULL && currNode->right == NULL)
+  {
+    if(leafDepth == -1)
+    {
+      //set the leaf depth
+      leafDepth = depth;
+      return true;
+    }
+    //compare depth
+    return(depth == leafDepth);
+  }
+
+  //recursive call to check the following trees 
+  return(helper(currNode->left, depth+1, leafDepth) && helper(currNode->right, depth+1, leafDepth));
 }
 
